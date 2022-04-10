fce04c492a82bcdc279dbd75eee401c96e718070
Jor Sanders <it@jor.dev>
2022-04-10 18:06:22 +0200

    split into 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RootComponent } from './resume/root/root.component';
+import { HeaderComponent } from './resume/header/header.component';
+import { ContentComponent } from './resume/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
